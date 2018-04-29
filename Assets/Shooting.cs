cd72a6908d6970d9bdd0877bb2ef3a7ea3d35db4
Unknown <jackhterzi@gmail.com>
2018-04-29 16:43:41 -0400

    Movement and shooting functional

---
+++ Assets/Shooting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount > 0) {
+            if (Input.GetTouch(0).pressure >= 3) {
+                Debug.Log("first finger shoot");
+            }
+            if (Input.touchCount > 1) {
+                if (Input.GetTouch(1).pressure >= 3) {
+                    Debug.Log("second Finge shoot");
+                }
+            }
+        }
+	}
+}
