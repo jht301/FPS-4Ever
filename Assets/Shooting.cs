231018f6e357990a769725578902cd1583132221
Unknown <jackhterzi@gmail.com>
2018-04-29 17:24:23 -0400

    working in main

--- Assets/Shooting.cs
+++ Assets/Shooting.cs
@@ -3,21 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Shooting : MonoBehaviour {
-
+    public AudioClip r;
+    float timer;
 	// Use this for initialization
 	void Start () {
-		
+        timer = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        timer -= Time.deltaTime;
         if (Input.touchCount > 0) {
-            if (Input.GetTouch(0).pressure >= 3) {
-                Debug.Log("first finger shoot");
+            if (Input.GetTouch(0).pressure >= 3&& timer <=0) {
+                SoundManager.me.Play(r);
+                timer = .1f;
+
             }
             if (Input.touchCount > 1) {
-                if (Input.GetTouch(1).pressure >= 3) {
-                    Debug.Log("second Finge shoot");
+                if (Input.GetTouch(1).pressure >= 3 && timer <=0) {
+                    SoundManager.me.Play(r);
+                    timer = .1f;
                 }
             }
         }
